b51a5739b0e625a269867c8d0b951b9787931f69
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-14 17:09:03 -0400

    Declare some properties "fields"

--- src/main/java/com/jay/bank_app/controller/AccountApp.java
+++ src/main/java/com/jay/bank_app/controller/AccountApp.java
@@ -1,4 +1,24 @@
 package com.jay.bank_app.controller;
 
+import javax.persistence.*;
+
+@Entity
 public class AccountApp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+    private String streetAddress;
+    private String city;
+    private String state;
+    private String country;
+    private String phoneNumber;
+    private Integer accountNumber;
+    private String accountType;
+    private Long accountBalance;
+    private Integer amountDeposited;
+    private Integer amountWithdrawn;
 }
