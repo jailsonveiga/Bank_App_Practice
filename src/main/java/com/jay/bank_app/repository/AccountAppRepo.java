c64e93cf79ba8cd5391817c3639ab5794a7ad4e4
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-14 21:11:16 -0400

    Extends JpaRepository class and datatype for the id

--- src/main/java/com/jay/bank_app/repository/AccountAppRepo.java
+++ src/main/java/com/jay/bank_app/repository/AccountAppRepo.java
@@ -1,4 +1,7 @@
 package com.jay.bank_app.repository;
 
-public interface AccountAppRepo{
+import com.jay.bank_app.model.AccountAppModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountAppRepo extends JpaRepository<AccountAppModel, Long> {
 }
